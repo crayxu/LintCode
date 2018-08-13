09bcff9be06c49eec72635ffe1b8d688755c37d5
Cray Xu <36046108+crayxu@users.noreply.github.com>
2018-08-12 22:30:41 -0400

    Add files via upload

---
+++ C++/search-a-2d-matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    /**
+     * @param matrix: matrix, a list of lists of integers
+     * @param target: An integer
+     * @return: a boolean, indicate whether matrix contains target
+     */
+    bool searchMatrix(vector<vector<int>> &matrix, int target) {
+        // write your code here
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+                
+            }
+        }
+        return false;
+    }
+};

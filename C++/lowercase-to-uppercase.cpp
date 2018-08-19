5ad108734b2cbb813bc76f8e307f4daac3af272f
Cray Xu <36046108+crayxu@users.noreply.github.com>
2018-08-19 18:05:50 -0400

    Add files via upload

---
+++ C++/lowercase-to-uppercase.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    /**
+     * @param character: a character
+     * @return: a character
+     */
+    char lowercaseToUppercase(char character) {
+        // write your code here
+        char result = character-32;
+        return result;
+    }
+};

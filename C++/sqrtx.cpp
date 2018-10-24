3de8b12f95d9ccfc422fa096bde45572d47f6493
Cray Xu <36046108+crayxu@users.noreply.github.com>
2018-10-24 11:33:46 -0400

    Create sqrtx.cpp

---
+++ C++/sqrtx.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    /**
+     * @param x: An integer
+     * @return: The sqrt of x
+     */
+    int sqrt(int x) {
+        if (x == 0) {
+            return 0;
+        } else {
+            long start = 1;
+            long end = x;
+            long mid;
+            while (start+1 < end) {
+                mid = start + (end-start)/2;
+                if (mid * mid > x) {
+                    end = mid;
+                }
+                start = mid;
+            }
+            if (end * end <= x) {
+                return (int) end;
+            }
+            return (int) start;
+        }
+    }
+};

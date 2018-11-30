92aa3e345369fff4acf8b9be5f11d1e27db09fe9
Cray Xu <36046108+crayxu@users.noreply.github.com>
2018-11-30 14:38:50 -0500

    Create simple-calculator.java

---
+++ C++/simple-calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+    /**
+     * @param a: An integer
+     * @param operator: A character, +, -, *, /.
+     * @param b: An integer
+     * @return: The result
+     */
+    public int calculate(int a, char operator, int b) {
+        int res = 0;
+        
+        switch (operator) {
+            case '+':
+                res = a+b;
+                break;
+            
+            case '-':
+                res = a-b;
+                break;
+            
+            case '*':
+                res = a*b;
+                break;
+            
+            case '/':
+                res = a/b;
+                break;
+        }
+        return res;
+    }
+}

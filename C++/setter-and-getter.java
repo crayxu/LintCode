d5e2538fc855975ac00bd4d8a62c0dfabec428fa
Cray Xu <36046108+crayxu@users.noreply.github.com>
2018-11-30 13:46:12 -0500

    Create setter-and-getter.java

---
+++ C++/setter-and-getter.java
@@ -0,0 +1,27 @@
+public class School {
+    /*
+     * Declare a private attribute *name* of type string.
+     */
+    // write your code here
+
+    /**
+     * Declare a setter method `setName` which expect a parameter *name*.
+     */
+    // write your code here
+
+    /**
+     * Declare a getter method `getName` which expect no parameter and return
+     * the name of this school
+     */
+    // write your code here
+    private String name;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+     
+}

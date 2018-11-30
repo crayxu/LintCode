636c44cd8215ebe3e23f18423fbdcaa93a3e1603
Cray Xu <36046108+crayxu@users.noreply.github.com>
2018-11-30 14:10:17 -0500

    Create generate-arraylist-with-given-size.js

---
+++ C++/generate-arraylist-with-given-size.js
@@ -0,0 +1,11 @@
+/**
+ * @param size: An integer
+ * @return: An integer list
+ */
+const generate = function (size) {
+    var res = [];
+    for (let i = 1; i < size+1; i++) {
+        res.push(i);
+    }
+    return res;
+}
